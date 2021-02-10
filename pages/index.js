9c43c7546968da22261c624119afd62a00336b25
MarcosCursino <marcos.cursino@hotmail.com>
2021-02-09 21:20:22 -0300

    ajustado estrutura do projeto

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-function pages() {
-  return <h1>Teste</h1>
-}
-
-export default pages;
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import * as S from '../styles/index';
+
+function Index() {
+  return <S.Title>Teste</S.Title>
+}
+
+export default Index;
